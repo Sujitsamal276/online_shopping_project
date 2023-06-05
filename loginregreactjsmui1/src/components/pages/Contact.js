4b1a7f6382df6f062897a719e75f1384b3b36d56
Sujitsamal276 <134088138+Sujitsamal276@users.noreply.github.com>
2023-06-05 20:02:21 +0530

    online_shopping_project

---
+++ loginregreactjsmui1/src/components/pages/Contact.js
@@ -0,0 +1,19 @@
+import { Grid } from "@mui/material"
+
+const Contact = () => {
+    return <>
+    <Grid container justifyContent='center'>
+        <Grid item sm={10}>
+        <h1>Contact Page</h1> 
+    <hr />
+    <p>Contact Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates vero aliquid reiciendis dignissimos, velit delectus vel corrupti, impedit quae earum mollitia iusto atque aliquam distinctio quasi rem? Delectus repellat dolor veniam aut praesentium architecto minima excepturi dicta ipsam incidunt, porro aliquam, error obcaecati eos non rem, itaque voluptatum voluptatem. Temporibus recusandae inventore esse optio accusantium officia tempora mollitia ullam iste, architecto quis, vel incidunt eligendi. Exercitationem maiores alias accusantium, ea, perspiciatis tempore a fugiat ex voluptatibus vel dignissimos voluptas illum obcaecati voluptatem quo? Delectus eum, quo animi alias minus perspiciatis dolorum similique impedit veniam, ex provident, voluptatem necessitatibus? Itaque, maxime?</p>
+        </Grid>
+    </Grid>
+   
+   </>
+}
+   
+  
+
+
+export default Contact
---
+++ loginregreactjsmui1/src/components/pages/Layout.js
@@ -0,0 +1,13 @@
+import { CssBaseline } from '@mui/material'
+import Navbar from './Navbar'
+import { Outlet } from 'react-router-dom'
+
+const Layout = () => {
+  return <>
+  <CssBaseline />
+  <Navbar />
+  <Outlet />
+  </>
+}
+
+export default Layout
